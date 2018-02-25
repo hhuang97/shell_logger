bcae5c869733c941e9850d311288b289f127eefb
hhuang74 <hhuang74@illinois.edu>
2018-02-24 20:50:26 -0600

    Add zsh hooks for thefuck

---
+++ client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"github.com/nvbn/shell_logger/client/shell"
+)
+
+func configure() {
+	clientPath, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	sh, err := shell.Get()
+	if err != nil {
+		panic(err)
+	}
+
+	if shell.InWrapper() {
+		fmt.Println(sh.SetupHooks(clientPath))
+	} else {
+		fmt.Println(sh.SetupWrapper(clientPath))
+	}
+}
+
+func main() {
+	mode := flag.String("mode", "", "configure|wrapper|submit")
+
+	flag.Parse()
+
+	switch *mode {
+	case "configure":
+		configure()
+	case "wrapper":
+		fmt.Println("wrapper")
+	case "submit":
+        fmt.Println("success cmd: " + shell.GetSuccessfulCommand())
+        fmt.Println("failed cmd: " + shell.GetFailedCommand())
+	default:
+		flag.Usage()
+		os.Exit(2)
+	}
+
+}
+
